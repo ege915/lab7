8fea4696c0800ea36dd2d9e0f138f13d3abd7e65
Elizabeth Ge <ege1915@gmail.com>
2023-05-22 20:50:18 -0700

    edited file

--- ListExamples.java
+++ ListExamples.java
@@ -17,7 +17,7 @@ class ListExamples {
     }
     return result;
   }
-
+//fixed
 
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
